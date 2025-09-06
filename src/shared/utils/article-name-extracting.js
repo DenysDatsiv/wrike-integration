20e43531ead6ba25174e7bc3462dc2f7c6c7d209
Denys Datsiv <dendatsiv@gmail.com>
2025-09-06 21:08:07 +0300

    dasd

--- src/shared/utils/article-name-extracting.js
+++ src/shared/utils/article-name-extracting.js
@@ -1,9 +1,9 @@
-// export function extractFileNameFromUrl(url) {
-//     const pathMatch = url.match(/\/article\/([^/]+)\/detail/);
-//     if (!pathMatch || pathMatch.length < 2) {
-//         throw new Error('Invalid URL format or missing "/article/xxx/detail" in the URL');
-//     }
-//     const fileName = pathMatch[1];
-//     const sanitizedFileName = fileName.replace(/[^\w\s]/gi, '_');
-//     return sanitizedFileName;
-// }
+export function extractFileNameFromUrl(url) {
+    const pathMatch = url.match(/\/article\/([^/]+)\/detail/);
+    if (!pathMatch || pathMatch.length < 2) {
+        throw new Error('Invalid URL format or missing "/article/xxx/detail" in the URL');
+    }
+    const fileName = pathMatch[1];
+    const sanitizedFileName = fileName.replace(/[^\w\s]/gi, '_');
+    return sanitizedFileName;
+}
